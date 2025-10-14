3e25e2e99e5ab9d0342d526661727954e949c8e4
Louiesdad42 <michael.chiodo@digipen.edu>
2025-10-14 13:07:39 -0700

    Prototype Movement and Projectiles

--- VGP Glizzy Hell/Assets/Scripts/Player Fire.cs
+++ VGP Glizzy Hell/Assets/Scripts/Player Fire.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class PlayerFire : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Tooltip("Put prefab to be fired here")]
+    public GameObject projectile;
+    [Tooltip("The minimum time between firing")]
+    public float cooldown = 0.3f;
+    private float timer = 0;
+    public float projSpeed = 10;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (timer < cooldown)
+        {
+            timer += Time.deltaTime;
+        }
+        else if (Input.GetMouseButton(0))
+        {
+            //calculate direction and fire
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0;
+            Fire((mousePos - transform.position).normalized);
+            timer = 0;
+        }
+    }
+
+    void Fire(Vector3 direction)
+    {
+        //create a clone of the prefab and set values
+        Vector3 spawnPos = transform.position + direction;
+        GameObject clone = Instantiate(projectile, spawnPos, Quaternion.LookRotation(Vector3.forward, direction));
+        clone.GetComponent<Rigidbody2D>().velocity = direction * projSpeed;
     }
 }
