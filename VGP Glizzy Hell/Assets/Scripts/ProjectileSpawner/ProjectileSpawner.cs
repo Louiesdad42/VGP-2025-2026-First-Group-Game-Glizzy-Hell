aaf4dc295cce4beca0d0522bc1f8986bd969cd10
DigiPenDevo <zachary.stout@digipen.edu>
2025-10-14 14:34:24 -0700

    Projectile and Projectile spawner
    Also added my own test scene

---
+++ VGP Glizzy Hell/Assets/Scripts/ProjectileSpawner/ProjectileSpawner.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class ProjectileSpawner : MonoBehaviour
+{
+    [SerializeField] Rigidbody2D Projectile;
+    [SerializeField] float SecondsBetweenSpawns;
+    [SerializeField] float XSpeed;
+    [SerializeField] float YSpeed;
+    float DeltaTracker = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        DeltaTracker += Time.deltaTime;
+        if (DeltaTracker >= SecondsBetweenSpawns)
+        {
+            DeltaTracker = 0.0f;
+            Rigidbody2D clone = Instantiate(Projectile, transform);
+            clone.velocity = new Vector2(XSpeed, YSpeed);
+        }
+    }
+}
